ef87ae2f76a311175b3be52f8a5bb9f06dd7e65e
Smith Junior <smith.junior@icloud.com>
2018-04-26 21:47:27 -0300

    Create a new migration  for to add the vendido collumn and descricao collumn on carro table!

---
+++ database/migrations/2018_04_126_033816_add_collumn_carros_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarcasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('carro', function (Blueprint $table) {
+            $table->boolean('vendido');
+            $table->text("descricao");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carro');
+    }
+}
